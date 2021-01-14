f8b202cb90e71abd9ebd75209d09b16208eef4d3
Casie Siekman <casiesiekman@Casies-MBP.localdomain>
2021-01-14 12:27:20 -0600

    update for hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+function App() {
   // Renders the entire app on the DOM
-  render() {
-    return (
-      <div>
-        <header className="App-header">
-          <h1>Random Giphy API</h1>
-        </header>
-        
-        <p>Results go here</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <header className="App-header">
+        <h1>Random Giphy API</h1>
+      </header>
+      
+      <p>Results go here</p>
+    </div>
+  );
 }
 
 export default App;
