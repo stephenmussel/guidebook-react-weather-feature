d8e96d39e480dd19b663cbfefeb09f3f28dd0ecf
phaydara <phaydara@gmail.com>
2022-07-09 16:48:01 -0500

    fetchData called on page load

--- src/components/FetchWeather/FetchWeather.jsx
+++ src/components/FetchWeather/FetchWeather.jsx
@@ -1,4 +1,18 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 function FetchWeather() {
+
+    const [results, setResults] = useState([]);
+
+    const fetchData = () => {
+        console.log('in fetchData!');
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
     return(
         <>
             <h2 style={{ padding: 20 }}>Results...</h2>
