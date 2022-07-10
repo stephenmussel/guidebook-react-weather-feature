13767d80a61d2cb645ebd7b660f122ff36958ed8
phaydara <phaydara@gmail.com>
2022-07-10 08:56:24 -0500

    search router receives search string

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -5,6 +5,10 @@ const router = express.Router();
 router.post('/', (req, res) => {
     console.log('in POST search router!');
 
+    const query = req.body.city;
+    console.log('query:', query);
+    
+
 });
 
 module.exports = router;
