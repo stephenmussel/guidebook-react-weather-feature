b79791dc634da02e371e121501f4f3303d95a416
phaydara <phaydara@gmail.com>
2022-07-10 08:26:17 -0500

    captured form click event

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,14 @@
 function Search() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('in handleSubmit!');
+        
+    }
+
     return(
-        <form>
-            <input placeholder="Search" style={{ marginLeft: 20, marginRight: 5 }}/>
+        <form onSubmit={handleSubmit}>
+            <input placeholder="City" style={{ marginLeft: 20, marginRight: 5 }}/>
             <input type="submit" value="Search" />
         </form>
     )
