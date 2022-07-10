02c318b7d32cdf860a8cb48a9de25413201c7e8a
phaydara <phaydara@gmail.com>
2022-07-10 08:31:47 -0500

    captures city input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
+
+
 function Search() {
+
+    const [city, setCity] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('in handleSubmit!');
-        
+
     }
 
     return(
+        <>
         <form onSubmit={handleSubmit}>
-            <input placeholder="City" style={{ marginLeft: 20, marginRight: 5 }}/>
+            <input 
+                placeholder="City" 
+                style={{ 
+                    marginLeft: 20, 
+                    marginRight: 5 
+                }}
+                value={city}
+                onChange={(event) => setCity(event.target.value)}
+            />
             <input type="submit" value="Search" />
         </form>
+        <p>{city}</p>
+        </>
     )
 }
 
