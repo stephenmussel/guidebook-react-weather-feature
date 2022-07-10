5e23b27c123977dfd97f87b7fab896cde3bfb65c
phaydara <phaydara@gmail.com>
2022-07-09 19:33:30 -0500

    added sagaMiddleware to applyMiddleware

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const store = createStore(
     combineReducers({
         weather,
     }),
-    applyMiddleware(logger),
+    applyMiddleware(logger, sagaMiddleware),
 );
 
 sagaMiddleware.run(rootSaga);
